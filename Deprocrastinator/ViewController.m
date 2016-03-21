a044ef96d08868b77fcc3902647a7379681523aa
Kyle <Ky@Kyles-MacBook-Pro.local>
2016-03-21 15:37:04 -0500

    git pushgit pushgit pushgit push

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -12,20 +12,28 @@
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property NSMutableArray *toDoText;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
+@property NSArray *tapColors;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.toDoText = [NSMutableArray arrayWithObjects:[UIColor redColor], [UIColor blueColor], nil];
+    self.toDoText = [NSMutableArray arrayWithObjects:@"test1", @"test2", nil];
+    
+    self.tapColors = [NSArray arrayWithObjects:[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], nil];
+    
+//    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
     cell.textLabel.text = [NSString stringWithFormat:@"%@", self.textField.text];
     cell.textLabel.text = [self.toDoText objectAtIndex:indexPath.row];
+//
+//    [tableView setEditing:YES animated:YES];
     
     return cell;
 }
@@ -37,7 +45,27 @@
 - (IBAction)onAddButtonPressed:(UIButton *)sender {
     [self.toDoText addObject:self.textField.text];
     [self.tableView reloadData];
-//    = [NSString stringWithFormat:@"%@", self.textField.text];
+    self.textField.text = @"";
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    cell.textLabel.textColor = [UIColor greenColor];
+}
+
+- (IBAction)editButtonClick:(id)sender {
+    if ([self.tableView isEditing] == NO){
+        [self.editButtonItem setTitle:@"Done"];
+        [self.tableView setEditing:YES animated:YES];
+   }else {
+       [self.editButtonItem setTitle:@"Edit"];
+       [self.tableView setEditing:NO animated:NO];
+       
+    }
+
+    
+}
+- Tableview
 
 @end
